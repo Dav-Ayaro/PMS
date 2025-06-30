2a894fc1e49c22430d535d10fdd61c98b54d7d11
Dav-Ayaro <dyabroad@gmail.com>
2025-06-30 16:55:23 +0300

    env,ignored some files and modified users model

--- PMS/urls.py
+++ PMS/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
+    path('onboarding/', include('onboarding.urls')),
+    path('communication/', include('communication.urls')),
+    path('fees/', include('fees.urls')),
+    path('complaints/', include('complaints.urls')),
+    path('reporting/', include('reporting.urls')),
+    path('', include('users.urls')),
 ]
